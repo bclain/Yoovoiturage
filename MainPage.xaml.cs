198ba41265d295e8e4afda00411cf6b7ab2c3b8e
Brice Clain <106686826+briceclain@users.noreply.github.com>
2022-11-23 10:14:32 -0500

    Ajoutez des fichiers projet.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ProjetFinal
+{
+    /// <summary>
+    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            mainFrame.Navigate(typeof(Trajets));
+        }
+
+        private void Button_Click_trajets(object sender, RoutedEventArgs e)
+        {
+
+            btnCompte.Style = (Style)this.Resources["ButtonMenNonActive"];
+            btnTrajets.Style = (Style)this.Resources["ButtonMenActive"];
+            this.mainFrame.Navigate(typeof(Trajets));
+        }
+
+        private void Button_Click_compte(object sender, RoutedEventArgs e)
+        {
+            btnTrajets.Style = (Style)this.Resources["ButtonMenNonActive"];
+            btnCompte.Style = (Style)this.Resources["ButtonMenActive"];
+            this.mainFrame.Navigate(typeof(Compte));
+        }
+    }
+}
