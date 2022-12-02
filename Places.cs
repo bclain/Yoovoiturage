38adc56ed3273aa2039ef7d934a9ce2c9aef14f3
Brice Clain <106686826+briceclain@users.noreply.github.com>
2022-12-01 23:47:58 -0500

    navig après connexion

--- Places.cs
+++ Places.cs
@@ -20,21 +20,29 @@ using System.Threading.Tasks;
             string heure_a;
             int nbPlaces;
             string statut;
+            string prix;
 
-            public Places(int id, string date, string typeVoiture, string nom_chauffeur, string heure_d, string arretd, string typePlace, string arreta, string heure_a, int nbPlaces, string statut)
+        public Places(int id, string date, string typeVoiture, string nom_chauffeur, string heure_d, string arretd, string typePlace, string arreta, string heure_a, int nbPlaces, string statut)
+        {
+            this.Id = id;
+            this.Date = date;
+            this.TypeVoiture = typeVoiture;
+            this.Nom_chauffeur = nom_chauffeur;
+            this.Heure_d = heure_d;
+            this.Arretd = arretd;
+            this.TypePlace = typePlace;
+            this.Arreta = arreta;
+            this.Heure_a = heure_a;
+            this.NbPlaces = nbPlaces;
+            this.Statut = statut;
+            if (this.typeVoiture == "VUS")
             {
-                this.Id = id;
-                this.Date = date;
-                this.TypeVoiture = typeVoiture;
-                this.Nom_chauffeur = nom_chauffeur;
-                this.Heure_d = heure_d;
-                this.Arretd = arretd;
-                this.TypePlace = typePlace;
-                this.Arreta = arreta;
-                this.Heure_a = heure_a;
-                this.NbPlaces = nbPlaces;
-                this.Statut = statut;
+                this.prix = "15$";
             }
+            else if (this.typeVoiture == "Berline") { 
+                this.prix = "10$";
+            }
+        }
 
             public int Id { get => id; set => id = value; }
             public string Date { get => date; set => date = value; }
@@ -47,8 +55,9 @@ using System.Threading.Tasks;
             public string Heure_a { get => heure_a; set => heure_a = value; }
             public int NbPlaces { get => nbPlaces; set => nbPlaces = value; }
             public string Statut { get => statut; set => statut = value; }
+            public string Prix { get => prix; set => prix = value; }
 
-            public override bool Equals(object obj)
+        public override bool Equals(object obj)
             {
                 return base.Equals(obj);
             }
